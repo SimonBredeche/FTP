172f6f981d381e38c8e5f5b47226ef3e916d1bc4
Simon Bredeche <simon.bredeche@u-psud.fr>
2021-12-09 11:25:01 +0100

    update main.js
    added args

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,8 @@
 import { launch } from "./client";
-launch(4242,"localhost")
-/*if(process.argv[2] != null && process.argv[3] != null){
+
+if(process.argv[2] != null && process.argv[3] != null){
     launch(process.argv[2],process.argv[3]);
 }
 else{
     console.log("missing arguments usage : node main.js <port> <host>")
-}*/
+}
--- server/src/main.js
+++ server/src/main.js
@@ -1,9 +1,9 @@
 import { launch } from "./server";
-launch(4242);
-/*if(process.argv[2] != null){
+
+if(process.argv[2] != null){
     console.log(`Starting serving on port : ${process.argv[2]}`)
     launch(process.argv[2]);
 }
 else{
     console.log("missing arguments usage : node main.js <port> ")
-}*/
+}
